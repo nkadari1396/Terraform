ab2dd3d2468aaa1e5979f4ec0069e9d5984e4536
Naveen Kadari <nkadari@Navs-iMac.local>
2025-03-04 13:24:23 +0800

    second commit

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,39 @@
-resource "aws_instance" "this" {
-  ami                     = "ami-09c813fb71547fc4f"
-  instance_type           = "t3.micro"
+resource "aws_instance" "terraform" {
 
+  ami                     =  var.ami_id
+  instance_type           =  var.instance_type
+  vpc_security_group_ids  = [aws_security_group.allow_ssh_terraform.id]
   tags = {
     Name = "Terraform"
+  }
+}
+
+
+
+resource "aws_security_group" "allow_ssh_terraform" {
+  name        = "allow_ssh"
+  description = "allow_ssh for port number 22"
+  
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] #allow from everyone
+    ipv6_cidr_blocks = ["::/0"]
+    }
+
+    tags = {
+    Name = "allow_ssh"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id"{
+    type = string
+    default = "ami-09c813fb71547fc4f"
+    description = "ami_id"
+}
+
+variable "instance_type"{
+    type = string
+    default = "t3.micro"
+    description = "name of the instance type"
+}
